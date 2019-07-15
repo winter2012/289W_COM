6e9a192f29ff2077662d117c956653ef1d91f37d
winter2012 <winter2012102@gmail.com>
2019-07-15 17:12:18 +0800

    first commit

---
+++ Public/Common/config.db.php
@@ -0,0 +1,13 @@
+<?php
+ return array (
+  'DB_HOST' => '192.168.0.166',
+  'DB_PORT' => '3306',
+  'DB_USER' => 'root',
+  'DB_NAME' => 'mitaodh_com',
+  'DB_PWD' => '123456',
+  'DB_PREFIX' => 'cc_',
+  'ROOT_FILE' => '/',
+  'DEFAULT_THEME' => 'Black',
+  'URL_MODEL' => '0',
+)
+?>
